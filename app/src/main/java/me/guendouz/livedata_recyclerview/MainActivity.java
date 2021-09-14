0b7794990c1e29d94c7f806ddbe24e8c69453dd0
Vineet K Jain <vkjain@gmail.com>
2021-09-14 17:54:08 +0530

    made changes for Banner Ads

--- app/src/main/java/me/guendouz/livedata_recyclerview/MainActivity.java
+++ app/src/main/java/me/guendouz/livedata_recyclerview/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,11 +12,20 @@ import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import me.guendouz.livedata_recyclerview.db.Post;
@@ -25,9 +35,12 @@ import me.guendouz.livedata_recyclerview.listener.DialogButtonClickListener;
 
 public class MainActivity extends AppCompatActivity implements ButtonClickListener {
 
-    private PostsAdapter postsAdapter;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> postsAdapter;
     private PostViewModel postViewModel;
     RecyclerView recyclerView;
+    private List<Object> recyclerViewItems = new ArrayList<>();
+    public static final int ITEMS_PER_AD = 2;
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/4177191030";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +59,9 @@ public class MainActivity extends AppCompatActivity implements ButtonClickListen
         // Create the observer which updates the UI.
         // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
         postViewModel.getAllPosts().observe(this, posts -> postsAdapter.setData(posts));
+        addBannerAds();
+        loadBannerAds();
+
     }
 
     @Override
@@ -90,4 +106,79 @@ public class MainActivity extends AppCompatActivity implements ButtonClickListen
     protected Context getContext() {
         return MainActivity.this;
     }
+
+    /**
+     * Adds banner ads to the items list.
+     */
+    private void addBannerAds() {
+        // Loop through the items array and place a new banner ad in every ith position in
+        // the items List.
+        for (int i = 0; i <= recyclerViewItems.size(); i += ITEMS_PER_AD) {
+            final AdView adView = new AdView(MainActivity.this);
+            adView.setAdSize(AdSize.BANNER);
+            adView.setAdUnitId(AD_UNIT_ID);
+            recyclerViewItems.add(i, adView);
+        }
+    }
+
+    /**
+     * Sets up and loads the banner ads.
+     */
+    private void loadBannerAds() {
+        // Load the first banner ad in the items list (subsequent ads will be loaded automatically
+        // in sequence).
+        loadBannerAd(0);
+    }
+
+    /**
+     * Loads the banner ads in the items list.
+     */
+    private void loadBannerAd(final int index) {
+
+        if (index >= recyclerViewItems.size()) {
+            return;
+        }
+
+        Object item = recyclerViewItems.get(index);
+        if (!(item instanceof AdView)) {
+            throw new ClassCastException("Expected item at index " + index + " to be a banner ad"
+                    + " ad.");
+        }
+
+        final AdView adView = (AdView) item;
+
+        // Set an AdListener on the AdView to wait for the previous banner ad
+        // to finish loading before loading the next ad in the items list.
+        adView.setAdListener(
+                new AdListener() {
+                    @Override
+                    public void onAdLoaded() {
+                        super.onAdLoaded();
+                        // The previous banner ad loaded successfully, call this method again to
+                        // load the next ad in the items list.
+                        loadBannerAd(index + ITEMS_PER_AD);
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(LoadAdError loadAdError) {
+                        // The previous banner ad failed to load. Call this method again to load
+                        // the next ad in the items list.
+                        String error =
+                                String.format(
+                                        "domain: %s, code: %d, message: %s",
+                                        loadAdError.getDomain(), loadAdError.getCode(), loadAdError.getMessage());
+                        Log.e(
+                                "MainActivity",
+                                "The previous banner ad failed to load with error: "
+                                        + error
+                                        + ". Attempting to"
+                                        + " load the next banner ad in the items list.");
+                        loadBannerAd(index + ITEMS_PER_AD);
+                    }
+                });
+
+        // Load the banner ad.
+        adView.loadAd(new AdRequest.Builder().build());
+    }
+
 }
--- app/src/main/java/me/guendouz/livedata_recyclerview/PostsAdapter.java
+++ app/src/main/java/me/guendouz/livedata_recyclerview/PostsAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdView;
+
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,10 +22,15 @@ import me.guendouz.livedata_recyclerview.listener.ButtonClickListener;
  * Created by guendouz on 15/02/2018.
  */
 
-public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHolder> {
+public class PostsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    // A menu item view type.
+    private static final int MENU_ITEM_VIEW_TYPE = 0;
 
-    private List<Post> data;
+    // The banner ad view type.
+    private static final int BANNER_AD_VIEW_TYPE = 1;
+
+    private List<Object> data;
     private Context context;
     private LayoutInflater layoutInflater;
     private ButtonClickListener onDeleteButtonClickListener;
@@ -35,15 +42,62 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHold
         this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    @Override
-    public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = layoutInflater.inflate(R.layout.layout_post_item, parent, false);
-        return new PostViewHolder(itemView);
+    /**
+     * The {@link AdViewHolder} class.
+     */
+    public class AdViewHolder extends RecyclerView.ViewHolder {
+
+        AdViewHolder(View view) {
+            super(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(PostViewHolder holder, int position) {
-        holder.bind(data.get(position));
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        switch (viewType){
+            case (MENU_ITEM_VIEW_TYPE):
+                View itemView = layoutInflater.inflate(R.layout.layout_post_item, viewGroup, false);
+                return new PostViewHolder(itemView);
+            case (BANNER_AD_VIEW_TYPE):
+                // fall through
+            default:
+                View bannerLayoutView = LayoutInflater.from(
+                        viewGroup.getContext()).inflate(R.layout.banner_ad_container,
+                        viewGroup, false);
+                return new AdViewHolder(bannerLayoutView);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        switch (viewType){
+            case MENU_ITEM_VIEW_TYPE:
+                PostViewHolder postViewHolder = (PostViewHolder) holder;
+                postViewHolder.bind((Post)data.get(position));
+            case BANNER_AD_VIEW_TYPE:
+                // fall through
+            default:
+                AdViewHolder bannerHolder = (AdViewHolder) holder;
+                AdView adView = (AdView) data.get(position);
+                ViewGroup adCardView = (ViewGroup) bannerHolder.itemView;
+                // The AdViewHolder recycled by the RecyclerView may be a different
+                // instance than the one used previously for this position. Clear the
+                // AdViewHolder of any subviews in case it has a different
+                // AdView associated with it, and make sure the AdView for this position doesn't
+                // already have a parent of a different recycled AdViewHolder.
+                if (adCardView.getChildCount() > 0) {
+                    adCardView.removeAllViews();
+                }
+                if (adView.getParent() != null) {
+                    ((ViewGroup) adView.getParent()).removeView(adView);
+                }
+
+                // Add the banner ad to the ad view.
+                adCardView.addView(adView);
+        }
     }
 
     @Override
@@ -51,7 +105,16 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHold
         return data.size();
     }
 
-    public void setData(List<Post> newData) {
+    /**
+     * Determines the view type for the given position.
+     */
+    @Override
+    public int getItemViewType(int position) {
+        return (position % MainActivity.ITEMS_PER_AD == 0) ? BANNER_AD_VIEW_TYPE
+                : MENU_ITEM_VIEW_TYPE;
+    }
+
+    public void setData(List<Object> newData) {
         if (data != null) {
             /*PostDiffCallback postDiffCallback = new PostDiffCallback(data, newData);
             DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(postDiffCallback);*/
